240c07098450b615a4dbcc557f5e03564f20bb33
Yash-Chauhan09 <yash848210@gmail.com>
2021-10-02 12:00:01 +0530

    Made the todos add to the localstorage so that todos can be save after page refresh.

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+// import ReactDOM from 'react-dom';
 
-
-const ToDoList = (props) =>{
-    
-    return(
-        <div>
-            <li className="todoList">{props.work}  <button className="delButton" onClick={()=>{
-                props.onSelect(props.id);
-            }}>Delete</button> </li>
-            <br/>
-            
-        </div>
-    );
-}
+const ToDoList = (props) => {
+  return (
+    <div>
+      <li className="todoList">
+        {props.work}{" "}
+        <button
+          className="delButton"
+          onClick={() => {
+            props.onSelect(props.id);
+          }}
+        >
+          Delete
+        </button>{" "}
+      </li>
+      <br />
+    </div>
+  );
+};
 
 export default ToDoList;
